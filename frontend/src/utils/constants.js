01571c1eef48b1a6015df711dbdd8460e86c239b
Sergey Petrosyan <syspect@bk.ru>
2023-11-28 04:57:34 +0500

    Refactor: производен рефакторинг кода

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 // Объект настроек для взаимодействия с сервером
 export const configApi = {
   // url: 'https://mesto.nomoreparties.co/v1/cohort-73',
-  // url: 'https://api.mesto.petrosyan.nomoredomainsmonster.ru',
-  url: 'http://localhost:3000',
+  url: 'https://api.mesto.petrosyan.nomoredomainsmonster.ru',
+  // url: 'http://localhost:3000',
 }
