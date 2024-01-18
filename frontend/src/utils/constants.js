4cbc5f2f8e334e42744aa9d0691ba4bd367ac9ed
Sergey Petrosyan <syspect@bk.ru>
2024-01-18 22:30:24 +0500

    hotfixes: в константе указан рабочий адрес бэкенда

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Объект настроек для взаимодействия с сервером
-export const BASE_URL = 'http://localhost:3002';
+// export const BASE_URL = 'http://localhost:3002';
 
-// export const BASE_URL = 'https://api.mesto.petrosyan.nomoredomainsmonster.ru';
+export const BASE_URL = 'https://api.mesto.petrosyan.nomoredomainsmonster.ru';
